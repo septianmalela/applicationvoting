3ec7452c44d303a4ed98eed01e5f34fa93a76f23
septianmalela <maulanas430@gmail.com>
2021-11-14 16:24:44 +0700

    update user page admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :set_angkatan, only: :update
+
   def index
     @users = User.order(active_user: :DESC)
   end
@@ -25,5 +27,30 @@ class Admin::UsersController < Admin::BaseController
       flash[:alert] = "#{@user.errors.messages}"
     end
     redirect_to admin_users_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user = @user.update(user_params)
+      redirect_to admin_user_path(params[:id])
+      flash[:notice] = 'User succesfully updated.'
+    else
+      flash[:alert] = @user.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:angkatan, :jadwal_vote_id, :email, :full_name)
+  end
+
+  def set_angkatan
+    params[:user][:angkatan] = params[:user][:angkatan].to_i
   end
 end
